e0c39ff73886b72b0ca16a60c2b7c8b40087a435
Adam-Barto <adam.jbarto@gmail.com>
2023-05-08 08:15:36 -0400

    urls in books created

--- booksapi/booksapi/urls.py
+++ booksapi/booksapi/urls.py
@@ -14,8 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # Api Url: Causes Error: ModuleNotFoundError: No module named 'books.urls'
+    # Notice the use of '.' rather than '/'
+    path('', include('books.urls'))
+
 ]
