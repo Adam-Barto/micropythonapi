f91ff10e599afa39d20840e0a017094d8a2cf7ac
Adam-Barto <adam.jbarto@gmail.com>
2023-05-08 08:02:18 -0400

    Views created, Error noted as Readme Foretold

--- booksapi/books/views.py
+++ booksapi/books/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+# views.py
+from rest_framework import viewsets
+
+from .serializers import BookSerializer
+from .models import Book
+
+
+# ModelViewSet is a special view that Django Rest Framework provides.
+# It will handle GET and POST for Books without us having to do any more work.
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all().order_by('name')
+    serializer_class = BookSerializer
